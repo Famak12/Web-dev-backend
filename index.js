f23f16ad4c505c01f8f22221b5c0bcab8d400711
Fatima Manzoor <fatimamnzkayani@gmail.com>
2024-09-25 13:01:26 +0500

    logic for random selection of activities created

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 // Step 1: Make sure that when a user visits the home page,
 //   it shows a random activity.You will need to check the format of the
 //   JSON data from response.data and edit the index.ejs file accordingly.
@@ -25,6 +26,21 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
+
+
+  try {
+    const type = req.body.type;
+    const participants =req.body.participants
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
+    const result = response.data;
+    res.render('index.ejs', {data: result[Math.floor(Math.random()* result.length)]});
+  } catch (error){
+    res.render('index.ejs', {
+      error: "No activities match your criteria"
+    })
+    }
+
+
   console.log(req.body);
 
   // Step 2: Play around with the drop downs and see what gets logged.
